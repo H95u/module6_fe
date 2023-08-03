d128a69c863d6ee4160fe47c905fb8e99e05f741
Minh Tran <minh.tv1411@gmail.com>
2023-08-03 10:14:08 +0700

    modal dvcc

--- src/components/user-info/PartnerInfo.js
+++ src/components/user-info/PartnerInfo.js
@@ -3,6 +3,32 @@ import {useParams} from "react-router-dom";
 import axios from "axios";
 import "./partnerprofile.css";
 
+function Modal() {
+    return (
+        <div className={"modal fade"} id={"exampleModal"} tabIndex="-1" aria-labelledby={"exampleModalLabel"}
+             aria-hidden={"true"}>
+            <div className={"modal-dialog"}>
+                <div className={"modal-content"}>
+                    <div className={"modal-header"}>
+                        <h5 className={"modal-title"} id={"exampleModalLabel"}>Cập nhật dịch vụ cung cấp</h5>
+                        <button className={"btn-close"} data-bs-dismiss={"modal"} aria-label={"Close"}></button>
+                    </div>
+                    <div className={"modal-body"}>
+                        <div className={"form-check form-switch"}>
+                            <input className={"form-check-input"} type={"checkbox"} id={"flexSwitchCheckDefault"}/>
+                                <label className={"form-check-label"} htmlFor={"flexSwitchCheckDefault"}>Đi chơi chung</label>
+                        </div>
+                    </div>
+                    <div className={"modal-footer"}>
+                        <button className={"btn btn-secondary"} data-bs-dismiss={"modal"}>Đóng</button>
+                        <button className={"btn btn-primary"}>Cập nhật</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
 export default function PartnerInfo() {
     const [user, setUser] = useState({});
     const [options, setOptions] = useState([]);
@@ -19,104 +45,110 @@ export default function PartnerInfo() {
     }, [])
 
     return (
-        <div className={"partner-info"}>
-            <div className={`partner-profile`}>
-                <div className={`d-flex justify-content-center`}>
-                    <div className={`cards`}>
-                        <div>
-                            <a href={`#`}><img src={user.img} alt={``}/></a>
+        <>
+            <div className={"partner-info"}>
+                <div className={`partner-profile`}>
+                    <div className={`d-flex justify-content-center`}>
+                        <div className={`cards`}>
+                            <div>
+                                <a href={`#`}><img src={user.img} alt={``}/></a>
+                            </div>
+                            <div><p className={"ready"}>Đang sẵn sàng</p></div>
+                            <div className={"dob"}><span>Ngày tham gia:</span><span><span>31/5/2019</span></span></div>
+                            <hr/>
                         </div>
-                        <div><p className={"ready"}>Đang sẵn sàng</p></div>
-                        <div className={"dob"}><span>Ngày tham gia:</span><span><span>31/5/2019</span></span></div>
-                        <hr/>
-                    </div>
-                    <div className={"info"}>
-                        <div className={`row`} id={`inner-info`}>
-                            <div className={"col-lg-8"}>
-                                <h2>{user.nickname}</h2>
-                            </div>
-                            <div className={"col-lg-4"}>
-                                <button><i className={"bi bi-heart"}></i> Theo dõi</button>
-                            </div>
-                        </div>
-                        <div className={"row"}>
-                            <div className={"col-md-3 col-xs-6"}>
-                                <div className={"item-nav-name"}>
-                                    <span>Số lượt xem</span>
+                        <div className={"info"}>
+                            <div className={`row`} id={`inner-info`}>
+                                <div className={"col-lg-7"}>
+                                    <h2>{user.nickname}</h2>
                                 </div>
-                                <div className={"item-nav-value"}>
-                                    <span>{user.viewCount} lượt</span>
+                                <div className={"col-lg-5"}>
+                                    <button className={`btn btn-success`} data-bs-toggle={"modal"}
+                                            data-bs-target={"#exampleModal"}>Cập nhật dịch vụ cung cấp
+                                    </button>
                                 </div>
                             </div>
-                            <div className={"col-md-3 col-xs-6"}>
-                                <div className={"item-nav-name"}>
-                                    <span>Số lượt thuê</span>
-                                </div>
-                                <div className={"item-nav-value"}>
-                                    <span>{user.rentCount} lượt</span>
-                                </div>
-                            </div>
-                            <div className={"col-md-3 col-xs-6"}>
-                                <div className={"item-nav-name"}>
-                                    <span>Ngày sinh</span>
-                                </div>
-                                <div className={"item-nav-value"}>
-                                    <span>{user.dob}</span>
-                                </div>
-                            </div>
-                            <div className={"col-md-3 col-xs-6"}>
-                                <div className={"item-nav-name"}>
-                                    <span>Địa chỉ</span>
-                                </div>
-                                <div className={"item-nav-value"}>
-                                    <span>{address.name}</span>
-                                </div>
-                            </div>
-                        </div>
-                        <hr/>
-                        <div className={"option"}>
-                            <h2>Dịch vụ</h2>
-                            <div className={`row`}>
-                                {options.map(item => (
-                                    <div className={`service-name`}>
-                                        <a className={"btn btn-outline-danger"}>{item.name}</a>
+                            <div className={"row"}>
+                                <div className={"col-md-3 col-xs-6"}>
+                                    <div className={"item-nav-name"}>
+                                        <span>Số lượt xem</span>
                                     </div>
-                                ))}
+                                    <div className={"item-nav-value"}>
+                                        <span>{user.viewCount} lượt</span>
+                                    </div>
+                                </div>
+                                <div className={"col-md-3 col-xs-6"}>
+                                    <div className={"item-nav-name"}>
+                                        <span>Số lượt thuê</span>
+                                    </div>
+                                    <div className={"item-nav-value"}>
+                                        <span>{user.rentCount} lượt</span>
+                                    </div>
+                                </div>
+                                <div className={"col-md-3 col-xs-6"}>
+                                    <div className={"item-nav-name"}>
+                                        <span>Ngày sinh</span>
+                                    </div>
+                                    <div className={"item-nav-value"}>
+                                        <span>{user.dob}</span>
+                                    </div>
+                                </div>
+                                <div className={"col-md-3 col-xs-6"}>
+                                    <div className={"item-nav-name"}>
+                                        <span>Địa chỉ</span>
+                                    </div>
+                                    <div className={"item-nav-value"}>
+                                        <span>{address.name}</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <hr/>
+                            <div className={"option"}>
+                                <h2>Dịch vụ</h2>
+                                <div className={`row`}>
+                                    {options.map(item => (
+                                        <div className={`service-name`}>
+                                            <a className={"btn btn-outline-danger"}>{item.name}</a>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                            <hr/>
+                            <div className={"profile"}>
+                                <h2>Thông tin</h2>
+                                <div className={"album-of-player"}>
+                                    <a>
+                                        <img
+                                            src="https://playerduo.net/api/upload-service/thumbs/medium/66f8b716-ee52-4590-aa0a-73bd28590f5f__2c6a5460-2cb6-11ee-a657-a54d6be1d46a__player_album.jpg"
+                                            alt=""/>
+                                    </a>
+                                </div>
+                                <p></p>
+                                <p>- Giọng bắc</p>
+                                <p>- Mng thuê ủng hộ tui đóng tiền đi học nha ^^</p>
+                                <p></p>
+                                <p>🤍 Liên minh huyền thoại ( Ad, Sp, Mid lo được )</p>
+                                <p></p>
+                                <p>🤍 Valorant ,CS GO ( chơi được từ bkim đổ xuống )</p>
+                                <p></p>
+                                <p>🤍 Naraka ( top 1 ez )</p>
+                                <p></p>
+                                <p>🤍 Onl Camx5</p>
+                                <p></p>
+                                <p>🤍 ... game gì cũng chơi</p>
                             </div>
                         </div>
-                        <hr/>
-                        <div className={"profile"}>
-                            <h2>Thông tin</h2>
-                            <div className={"album-of-player"}>
-                                <a>
-                                    <img
-                                        src="https://playerduo.net/api/upload-service/thumbs/medium/66f8b716-ee52-4590-aa0a-73bd28590f5f__2c6a5460-2cb6-11ee-a657-a54d6be1d46a__player_album.jpg"
-                                        alt=""/>
-                                </a>
+                        <div className={"action"}>
+                            <h1>80.000đ/h</h1>
+                            <div className={`booking`}><a className={"btn btn-danger"}>THUÊ</a></div>
+                            <div><a className={"btn btn-light"}>TẶNG TIỀN</a></div>
+                            <div><a className={"btn btn-light"}><i className={"bi bi-chat-square-fill"}></i> CHÁT</a>
                             </div>
-                            <p></p>
-                            <p>- Giọng bắc</p>
-                            <p>- Mng thuê ủng hộ tui đóng tiền đi học nha ^^</p>
-                            <p></p>
-                            <p>🤍 Liên minh huyền thoại ( Ad, Sp, Mid lo được )</p>
-                            <p></p>
-                            <p>🤍 Valorant ,CS GO ( chơi được từ bkim đổ xuống )</p>
-                            <p></p>
-                            <p>🤍 Naraka ( top 1 ez )</p>
-                            <p></p>
-                            <p>🤍 Onl Camx5</p>
-                            <p></p>
-                            <p>🤍 ... game gì cũng chơi</p>
                         </div>
-                    </div>
-                    <div className={"action"}>
-                        <h1>80.000đ/h</h1>
-                        <div className={`booking`}><a className={"btn btn-danger"}>THUÊ</a></div>
-                        <div><a className={"btn btn-light"}>TẶNG TIỀN</a></div>
-                        <div><a className={"btn btn-light"}><i className={"bi bi-chat-square-fill"}></i> CHÁT</a></div>
                     </div>
                 </div>
             </div>
-        </div>
+            <Modal/>
+        </>
     )
 }
