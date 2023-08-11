8aa45dac5459af801d6d44c82eccbb97193a6175
Lam <lamvi1522@gmail.com>
2023-08-11 08:43:58 +0700

    create_7_feedback_service

--- src/services/feedback.service.js
+++ src/services/feedback.service.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 class FeedbackService {
-    async static getFeedbackByReceiverId(id) {
+    static async getFeedbackByReceiverId(id) {
         return await axios.get(`http://localhost:8080/api/feedbacks/receiver/${id}`)
     }
 
-    async static createFeedBack(data) {
+    static async createFeedBack(data) {
         return await axios.post(`http://localhost:8080/api/feedbacks/create`, data)
     }
 }
