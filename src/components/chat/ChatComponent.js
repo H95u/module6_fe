2503fd56004a7da0738d3e79b4048ae76a65f00d
Lam <lamvi1522@gmail.com>
2023-08-17 13:32:52 +0700

    update-album

--- src/components/chat/ChatComponent.js
+++ src/components/chat/ChatComponent.js
@@ -1,37 +1,135 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import "./Chat.css";
+import {useNavigate} from "react-router-dom";
+import axios from "axios";
+import stompClient from "../../config/socket";
 
 const ChatForm = () => {
+    const loggingUser = JSON.parse(localStorage.getItem("loggingUser"));
+    const [message, setMessage] = useState([]);
+    const [selectedSender, setSelectedSender] = useState(null);
+    const [messageInput, setMessageInput] = useState("");
+
+    const handleSenderClick = (senderId) => {
+        setSelectedSender(senderId);
+    };
+
+    const getMessage = () => {
+        if (loggingUser != null) {
+            const id = loggingUser.id;
+            axios.get(`http://localhost:8080/api/messages/user/${id}`).then((response) => {
+                setMessage(response.data);
+                console.log(response.data);
+                console.log(message.sender);
+            });
+        }
+    };
+
+    const handleSubmitChat = () => {
+        if (messageInput.trim() !== "") {
+            const data = {
+                content: messageInput,
+                sender: {
+                    id: loggingUser.id,
+                },
+                receiver: {
+                    id: selectedSender,
+                },
+            };
+
+            sendMessage(JSON.stringify(data));
+            setMessageInput("");
+        }
+    };
+
+    useEffect(() => {
+        getMessage();
+
+        stompClient.connect({}, () => {
+            console.log("STOMP Connected");
+            stompClient.subscribe(`/topic/messages/chat`, (data) => {
+                const receivedMessage = JSON.parse(data.body);
+                setMessage(prevMessages => [...prevMessages, receivedMessage]);
+            }, {});
+        }, (error) => {
+            console.error("STOMP Connection Error:", error);
+        });
+        return () => {
+            stompClient.disconnect();
+        };
+    }, []);
+
+    const sendMessage = (data) => {
+        stompClient.send("/app/chat", {}, data);
+    };
 
 
     return (
         <div className="chat-container">
             <div className="sidebar-chat">
-                <h3>Chat Với</h3>
-                <ul>
-                    <li>
-                        <img src="" alt=""/>
-                    </li>
-                </ul>
+                <h3>
+                    Chat
+                </h3>
+                {/*<div className={"distance"}>*/}
+                {/*    {message.length !== 0 && message.map(item => (*/}
+                {/*        <div className={"content-chat"}>*/}
+                {/*            <img className={"avt-img-chat"} src={item.sender.img} alt=""/>*/}
+                {/*            <p onClick={() => handleSenderClick(item.sender.id)}>{item.sender.username}</p>*/}
+                {/*        </div>*/}
+                {/*    ))}*/}
+                {/*</div>*/}
+                <div className={"distance"}>
+                    {message.length !== 0 &&
+                        <div className={"content-chat"}>
+                            <img className={"avt-img-chat"} src={message.sender?.img} alt=""/>
+                            <p onClick={() => handleSenderClick(message.sender?.id)}>{message.sender?.username}</p>
+                        </div>
+                    }
+                </div>
             </div>
             <div className="chat-form-container">
                 <div className="chat-messages">
+                    {message.map(item => (
                         <div
-                            className="chat-message user-message other-message"
+                            key={item.id}
+                            className={`chat-message ${item.sender.id === loggingUser.id ? 'user-message' : 'other-message'}`}
                         >
-                            <div className="avatar user-avatar other-avatar"/>
-                            <p></p>
+                            <div
+                                className={`avatar ${item.sender.id === loggingUser.id ? 'user-avatar' : 'other-avatar'}`}>
+                                <img src={item.sender.img} alt=""/>
+                            </div>
+                            <p>{item.content}</p>
                         </div>
+                    ))}
                 </div>
+
                 <form className="message-input">
-                    <input
-                        type="text"
-                        placeholder="Type your message..."
-                    />
-                    <button className={"button-chat"}>
-                        Send
-                    </button>
+                    <div className="message-content">
+                        <input
+                            style={{width: 950}}
+                            type="text"
+                            placeholder="Nhập tin nhắn..."
+                            value={messageInput}
+                            onChange={(e) => setMessageInput(e.target.value)}
+                            onKeyDown={(e) => {
+                                if (e.key === 'Enter') {
+                                    e.preventDefault();
+                                    handleSubmitChat();
+                                }
+                            }}
+                        />
+                        <button className={"button-chat"} onClick={handleSubmitChat}>
+                            Gửi
+                        </button>
+                    </div>
+                    <div className="receiver-avatar">
+                        {selectedSender && (
+                            <img src={message.find(item => item.sender.id === selectedSender)?.sender.img} alt=""/>
+                        )}
+                    </div>
                 </form>
+
+
             </div>
         </div>
     );
--- src/components/message/Message.js
+++ src/components/message/Message.js
@@ -6,142 +6,15 @@ import {Typography} from "@mui/material";
 import {useNavigate} from "react-router-dom";
 
 export default function Message() {
-    const loggingUser = JSON.parse(localStorage.getItem("loggingUser"));
-
-    const [message, setMessage] = useState([]);
-    const [selectedSender, setSelectedSender] = useState(null);
-    const [messageInput, setMessageInput] = useState("");
     const navigate = useNavigate();
 
     const getChat = () => {
       navigate("/chat")
     }
 
-    const handleSenderClick = (senderId) => {
-        setSelectedSender(senderId);
-    };
-
-    const renderChatContent = () => {
-        if (!selectedSender) {
-            return <div>Chọn một người gửi để xem tin nhắn</div>;
-        }
-
-        const selectedMessages = message.filter((item) => item.sender.id === selectedSender);
-
-        if (selectedMessages.length === 0) {
-            return <div>Không tìm thấy tin nhắn của người gửi này</div>;
-        }
-
-        return (
-            <div>
-                {selectedMessages.map((msg) => (
-                    <div key={msg.id}>
-                        <p>{msg.timestamp}</p>
-                        <p>{msg.content}</p>
-                    </div>
-                ))}
-            </div>
-        );
-    };
-
-    const getMessage = () => {
-        if (loggingUser != null) {
-            const id = loggingUser.id;
-            axios.get(`http://localhost:8080/api/messages/user/${id}`).then((response) => {
-                setMessage(response.data);
-            });
-        }
-    };
-
-    const handleSubmitChat = () => {
-        if (messageInput.trim() !== "") {
-            const data = {
-                content: messageInput,
-                sender: {
-                    id: loggingUser.id,
-                },
-                receiver: {
-                    id: selectedSender,
-                },
-            };
-
-            sendMessage(JSON.stringify(data));
-            setMessageInput("");
-        }
-    };
-
-    useEffect(() => {
-        getMessage();
-
-        stompClient.connect({}, () => {
-            console.log("STOMP Connected");
-            stompClient.subscribe(`/topic/messages/${loggingUser.id}/${selectedSender}`, (data) => {
-                const receivedMessage = JSON.parse(data.body);
-                setMessage(prevMessages => [...prevMessages, receivedMessage]);
-            }, {});
-        }, (error) => {
-            console.error("STOMP Connection Error:", error);
-        });
-        return () => {
-            stompClient.disconnect();
-        };
-    }, []);
-
-    const sendMessage = (data) => {
-        stompClient.send("/app/chat", {}, data);
-    };
-
     return (
         <>
             <img onClick={getChat} id="chat-icon" src="/chat.png" alt="chat"/>
-            <Popover placement="left">
-                <PopoverContent id="chat-frame">
-                    <div className="gray-background">
-                        <Typography color={"blue"} variant={"h3"} textGradient>
-                            Tin nhắn
-                        </Typography>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-3 chat-left">
-                            <table className="table table-hover">
-                                <thead>
-                                Danh sách người gửi
-                                </thead>
-                                <tbody>
-                                {message.length !== 0 && message.map(item => (
-                                    <tr>
-                                        <th onClick={() => handleSenderClick(item.sender.id)}>{item.sender.username}</th>
-                                    </tr>
-                                ))}
-                                </tbody>
-                            </table>
-                        </div>
-                        <div className="col-md-9 chat-right">
-                            <div id={"content"}>
-                                {renderChatContent()}
-                            </div>
-                            <div className={"mt-80 flex items-center"}>
-                                <input className={"w-50"} placeholder="Nhập tin nhắn..." type="text" value={messageInput}
-                                       onChange={(e) => setMessageInput(e.target.value)}/>
-                                <button className={"send-btn ml-4"} onClick={handleSubmitChat}>
-                                    <div className="svg-wrapper-1">
-                                        <div className="svg-wrapper">
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24"
-                                                 height="24">
-                                                <path fill="none" d="M0 0h24v24H0z"></path>
-                                                <path fill="currentColor"
-                                                      d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"></path>
-                                            </svg>
-                                        </div>
-                                    </div>
-                                    <span>Gửi</span>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-
-                </PopoverContent>
-            </Popover>
         </>
     );
 }
